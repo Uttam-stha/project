4e521e1923960962df067d63b763395f4f6456e6
Uttam-stha <uttamshrestha9810@gmail.com>
2024-08-13 17:50:06 +0545

    hello

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Include the input-output stream library
+
+using namespace std;  // Use the standard namespace
+
+int main() {
+    cout << "Hello, World!";  // Print "Hello, World!" to the screen
+    return 0;  // Indicate that the program ended successfully
+}
